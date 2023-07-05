c63ffc6b010607307b281e26b96727ac5a5f0b97
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-07-05 12:24:36 +0200

    add validating by points

--- src/main/java/com/example/memorizing/service/FileService.kt
+++ src/main/java/com/example/memorizing/service/FileService.kt
@@ -1,24 +1,37 @@
 package com.example.memorizing.service
 
+import com.example.memorizing.entity.EWordStatus
 import com.example.memorizing.entity.Word
 import com.example.memorizing.entity.Words
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.File
 import java.io.FileInputStream
 
 @Service
-class FileService {
+class FileService(
+    @Value("\${completed.maxPoint}")
+    private val maxPoint: Int
+) {
     private val logger: Logger = LogManager.getLogger(FileService::class.java)
 
     private val mapper = ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true)
 
     init {
         loadWords()
+        validateWords()
         readNewWordsFromFile()
+    }
+
+    fun saveWords() {
+        val file = File(Words.fileName)
+        file.mkdirs()
+        file.delete()
+        mapper.writeValue(file, Words)
     }
 
     private fun loadWords() {
@@ -27,13 +40,6 @@ class FileService {
             mapper.readValue(file, Words::class.java)
         }
         saveWords()
-    }
-
-    fun saveWords() {
-        val file = File(Words.fileName)
-        file.mkdirs()
-        file.delete()
-        mapper.writeValue(file, Words)
     }
 
     private fun readNewWordsFromFile() {
@@ -57,5 +63,15 @@ class FileService {
             }
             saveWords()
         }
+    }
+
+    private fun validateWords() {
+        Words.mapOfWords.forEach {
+            val word = it.value
+            if (word.point < 0 && word.status != EWordStatus.HARD) it.value.status = EWordStatus.HARD
+            if (word.point in 0 .. maxPoint && word.status != EWordStatus.NORMAL) it.value.status = EWordStatus.NORMAL
+            if (word.point > maxPoint && word.status != EWordStatus.COMPLETED) it.value.status = EWordStatus.COMPLETED
+        }
+        saveWords()
     }
 }
