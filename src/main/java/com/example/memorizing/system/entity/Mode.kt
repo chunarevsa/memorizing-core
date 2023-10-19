47819865785bc9f77c9c5d42df69d4d99f6d3d7d
Chunarev Sergei <chunarev.se.a@gmail.com>
2023-10-19 16:52:37 +0200

    cleaning

--- src/main/java/com/example/memorizing/system/entity/Mode.kt
+++
@@ -1,9 +0,0 @@
-package com.example.memorizing.system.entity
-
-//import com.example.memorizing.card.EModeType
-
-//data class Mode(
-//    val fromKey: Boolean?,
-//    val modeType: EModeType,
-//    val listOfCardStatus: ArrayList<ECardStatus>,
-//)
