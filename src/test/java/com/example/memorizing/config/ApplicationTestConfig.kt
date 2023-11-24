5b362803d70dae9b36cebc06b76e01b961b5a79f
Chunarev Sergey <sergey.chunarev@ural.rt.ru>
2023-11-24 20:17:05 +0500

    temp

--- src/test/java/com/example/memorizing/config/ApplicationTestConfig.kt
+++ src/test/java/com/example/memorizing/config/ApplicationTestConfig.kt
@@ -1,13 +1,13 @@
 package com.example.memorizing.config
 
-import org.mockito.MockitoAnnotations
-import org.springframework.boot.test.context.TestConfiguration
-
-@TestConfiguration
-open class ApplicationTestConfig {
-
-    init {
-        MockitoAnnotations.openMocks(this)
-    }
-
-}
+//import org.mockito.MockitoAnnotations
+//import org.springframework.boot.test.context.TestConfiguration
+//
+//@TestConfiguration
+//open class ApplicationTestConfig {
+//
+//    init {
+//        MockitoAnnotations.openMocks(this)
+//    }
+//
+//}
