86300047fa5a9854e34b4a47546139a4b46541ec
Chunarev Sergey <sergey.chunarev@ural.rt.ru>
2023-11-25 20:18:55 +0500

    temp

--- src/test/java/com/example/memorizing/MemorizingApplicationTests.java
+++ src/test/java/com/example/memorizing/MemorizingApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.memorizing;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MemorizingApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MemorizingApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
