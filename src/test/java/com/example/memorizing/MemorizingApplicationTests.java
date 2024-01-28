0a48182aaea8aa877f580aca42d9f9eca3535ee0
Chunarev Sergey <sergey.chunarev@ural.rt.ru>
2024-01-28 22:36:02 +0500

    fix

--- src/test/java/com/example/memorizing/MemorizingApplicationTests.java
+++ src/test/java/com/example/memorizing/MemorizingApplicationTests.java
@@ -1,14 +1,15 @@
 package com.example.memorizing;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class MemorizingApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 
 }
